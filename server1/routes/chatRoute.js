a024a1a52e09f170b752b2f6b682dcd58120bb30
Taofeek Olusola <olusolataofeek@gmail.com>
2025-06-04 21:37:56 +0100

    hackathon

--- server1/routes/chatRoute.js
+++ server1/routes/chatRoute.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
-const { verifyToken } = require('../middleware/auth');
 
 // Route to handle medical chat
-router.post('/medical', verifyToken, chatController.handleMedicalChat);
+router.post('/medical', chatController.handleMedicalChat);
 
 module.exports = router;
